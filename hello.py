bb2a2e4996d101613b4ecb947b836e5b5344da89
samzhuwj <samzhuwj@gmail.com>
2018-02-06 09:09:05 +0800

    timestamp rendering with Flask-Moment

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
@@ -9,7 +10,8 @@ moment = Moment(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',
+                           current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
