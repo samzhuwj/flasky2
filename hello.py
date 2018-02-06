af70521c6dbd2e1eabc4d6169e223e24cb2fddf3
samzhuwj <samzhuwj@gmail.com>
2018-02-06 10:41:54 +0800

    class NameForm

--- hello.py
+++ hello.py
@@ -1,7 +1,11 @@
 from datetime import datetime
+
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 
@@ -29,3 +33,8 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
