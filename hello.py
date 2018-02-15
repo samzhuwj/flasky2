d1df7313b9e895a4600c7c0868456ae37f0eff47
samzhuwj <samzhuwj@gmail.com>
2018-02-15 23:49:21 +0800

    asynchronous email support

--- hello.py
+++ hello.py
@@ -6,6 +6,7 @@ from flask_migrate import Migrate
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
+from threading import Thread
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
@@ -93,9 +94,16 @@ def index():
                            known=session.get('known', False))
 
 
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
 def send_email(to, subject, template, **kwargs):
     msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
                   sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    mail.send(msg)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
